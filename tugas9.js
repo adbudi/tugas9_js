512692da8388c3a867e385b7e6c6ebb0a0040f3b
as <adbudi1@gmail.com>
2021-07-24 13:39:11 +0700

    tugas 9 javascript
    tugas ini di kumpulkan untuk niomic

---
+++ tugas9.js
@@ -0,0 +1,15 @@
+function nilaiObject() {
+  var table ={
+    nama : 'Arman Maulana',
+    nis :  7009,
+      semester1 : 2.5,
+      semester2 : 3,
+      semester3 : 2.9,
+   }
+    for (var x in table) {
+    console.log(table[x])
+
+  }
+
+}
+nilaiObject()
